b8d2462fe403ff006946b12a067c236e63231092
Sven Niederberger <s-niederberger@outlook.com>
2021-02-02 21:16:06 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![forbid(unsafe_code)]
+#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
+#![warn(clippy::all, rust_2018_idioms)]
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    let app = egui_template::TemplateApp::default();
+    eframe::run_native(Box::new(app));
+}
